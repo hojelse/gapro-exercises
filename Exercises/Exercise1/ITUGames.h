7f63124614b35d9c458c7ac471327f9c86e786ee
Chris <carve.chris@gmail.com>
2023-08-29 20:18:04 +0200

    added description, license and supported terminals to ITUGame.h

--- Exercises/Exercise1/ITUGames.h
+++ Exercises/Exercise1/ITUGames.h
@@ -1,3 +1,44 @@
+/*
+ * ITUGames.h
+ * simple library for
+ * 
+ * Supported terminals:
+ * - win: cmd, PowerShell
+ * - mac: ???
+ * Namespaces
+ * - `Console` contains simple and portable (as much as possible) functions for console rendering, without using external dependencie
+ * - `Utils` contains common gamedev utilities
+ * 
+ * Known issues:
+ * - `ITUGames::Console` functions may not work properly in emulated terminals.
+ * - Printing/clearing at high frequency (ie, in a loop at 60FPS) at the same terminal coordinates may cause some of the characters to skip rendering. adding `std::cout << std::flush;` forces the terminal to print all the characters before moving forward.
+ * 
+ * author: Chris-Carvelli
+ * 
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2023 Chris
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #pragma once
 
 #include <iostream>
