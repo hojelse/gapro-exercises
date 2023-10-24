0b0ff4a80a27b63706812dbec3bd2f44f179ca16
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2023-10-24 18:05:42 +0200

    Move Asteroids and Wolfenstein into srep

--- SimpleRenderEngineProject/Wolfenstein/ExampleGame/Game/ComponentLevelLayout.cpp
+++ SimpleRenderEngineProject/Wolfenstein/ExampleGame/Game/ComponentLevelLayout.cpp
@@ -1,4 +1,6 @@
 #include "ComponentLevelLayout.h"
+#include "ComponentRendererMesh.h"
+#include "Engine/MyEngine.h"
 
 #include "glm/gtx/transform.hpp"
 
@@ -10,6 +12,12 @@ void ComponentLevelLayout::Init(rapidjson::Value& serializedData) {
 	// 	for (int y = 0; y < dimy; y++)
 	// 		std::cout << serializedData["layout"][0][0].GetInt() << std::endl;
 
+	auto engine = MyEngine::Engine::GetInstance();
+
+	auto go_weak = engine->CreateGameObject("LayoutContainer");
+
+	// go_weak.lock()->AddComponent(std::shared_ptr<ComponentRendererMesh>());
+	go_weak.lock()->Init(serializedData);
 }
 
 void ComponentLevelLayout::Update(float deltaTime) {
