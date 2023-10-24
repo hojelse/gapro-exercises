a4bf2e855be52e536943d1922e6362d0263ad265
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2023-10-24 18:07:05 +0200

    Comment out broken ComponentLevelLayout::Init

--- SimpleRenderEngineProject/Wolfenstein/ExampleGame/Game/ComponentLevelLayout.cpp
+++ SimpleRenderEngineProject/Wolfenstein/ExampleGame/Game/ComponentLevelLayout.cpp
@@ -12,12 +12,12 @@ void ComponentLevelLayout::Init(rapidjson::Value& serializedData) {
 	// 	for (int y = 0; y < dimy; y++)
 	// 		std::cout << serializedData["layout"][0][0].GetInt() << std::endl;
 
-	auto engine = MyEngine::Engine::GetInstance();
+	// auto engine = MyEngine::Engine::GetInstance();
 
-	auto go_weak = engine->CreateGameObject("LayoutContainer");
+	// auto go_weak = engine->CreateGameObject("LayoutContainer");
 
 	// go_weak.lock()->AddComponent(std::shared_ptr<ComponentRendererMesh>());
-	go_weak.lock()->Init(serializedData);
+	// go_weak.lock()->Init(serializedData);
 }
 
 void ComponentLevelLayout::Update(float deltaTime) {
