c4ef4ac69f2c95bb57ce6afcca39a9cb81e35755
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2023-10-24 17:36:01 +0200

    Add textured cube to scene

--- Wolfenstein/ExampleGame/Game/ComponentRendererMesh.cpp
+++ Wolfenstein/ExampleGame/Game/ComponentRendererMesh.cpp
@@ -1,6 +1,7 @@
 #include "ComponentRendererMesh.h"
 
 #include "glm/gtx/transform.hpp"
+#include <unistd.h>
 
 void ComponentRendererMesh::Init(rapidjson::Value& serializedData) {
 	_mesh = sre::Mesh::create()
@@ -18,30 +19,44 @@ void ComponentRendererMesh::Init(rapidjson::Value& serializedData) {
 }
 
 void ComponentRendererMesh::Update(float deltaTime) {
-    //GetGameObject()->transform = glm::rotate(GetGameObject()->transform, glm::pi<float>() * deltaTime, glm::vec3(0, 1, 0));
+    // GetGameObject()->transform = glm::rotate(GetGameObject()->transform, glm::pi<float>() * deltaTime, glm::vec3(0, 1, 0));
 }
 
 void ComponentRendererMesh::Render(sre::RenderPass& renderPass) {
-    renderPass.draw(_mesh, GetGameObject()->transform, _material);
-    static auto cube = sre::Mesh::create().withCube(0.5f).build();
-    static std::vector<std::shared_ptr<sre::Material> > materials = {
-            sre::Shader::getUnlit()->createMaterial(),
-            sre::Shader::getUnlit()->createMaterial(),
-            sre::Shader::getUnlit()->createMaterial()
-    };
-
-    std::vector<glm::vec3> positions = {
-            {-1,0,-2},
-            { 0,0,-3},
-            { 1,0,-4}
-    };
-    std::vector<sre::Color> colors = {
-            {1,0,0,1},
-            {0,1,0,1},
-            {0,0,1,1},
-    };
-    for (int i = 0; i < positions.size(); i++) {
-        materials[i]->setColor(colors[i]);
-        renderPass.draw(cube, glm::translate(positions[i]), materials[i]);
+    for (int i = 0; i < 4; i++) {
+        renderPass.draw(
+            _mesh,
+            glm::rotate(
+                GetGameObject()->transform,
+                glm::pi<float>()/2 * i,
+                glm::vec3(0, 1, 0)
+            ),
+            _material
+        );
     }
+
+    // usleep(10000000);
+
+    // renderPass.draw(_mesh, GetGameObject()->transform, _material);
+    // static auto cube = sre::Mesh::create().withCube(0.5f).build();
+    // static std::vector<std::shared_ptr<sre::Material> > materials = {
+    //         sre::Shader::getUnlit()->createMaterial(),
+    //         sre::Shader::getUnlit()->createMaterial(),
+    //         sre::Shader::getUnlit()->createMaterial()
+    // };
+
+    // std::vector<glm::vec3> positions = {
+    //         {-1,0,-2},
+    //         { 0,0,-3},
+    //         { 1,0,-4}
+    // };
+    // std::vector<sre::Color> colors = {
+    //         {1,0,0,1},
+    //         {0,1,0,1},
+    //         {0,0,1,1},
+    // };
+    // for (int i = 0; i < positions.size(); i++) {
+    //     materials[i]->setColor(colors[i]);
+    //     renderPass.draw(cube, glm::translate(positions[i]), materials[i]);
+    // }
 }
